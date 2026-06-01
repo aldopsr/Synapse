982dd562bfdc3c3053799928ca3ae65aa9e566e0
unknown <mochammadalthofsr@gmail.com>
2026-06-01 14:11:06 +0700

    fix: set cloudinary config manually

--- synapse-backend/app/Providers/AppServiceProvider.php
+++ synapse-backend/app/Providers/AppServiceProvider.php
@@ -1,29 +1,23 @@
 <?php
-
 namespace App\Providers;
-
 use Illuminate\Support\ServiceProvider;
 use Laravel\Sanctum\Sanctum;
 use App\Models\PersonalAccessToken;
-
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-    //
+        //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
         $this->app->booted(function () {
             Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
         });
+
+        config([
+            'cloudinary.cloud_url' => 'cloudinary://' . env('CLOUDINARY_KEY') . ':' . env('CLOUDINARY_SECRET') . '@' . env('CLOUDINARY_CLOUD_NAME'),
+        ]);
     }
 }
