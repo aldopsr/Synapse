26c9cbab02df1f6f7dc8aa8c8384665ee6008878
unknown <mochammadalthofsr@gmail.com>
2026-06-03 11:09:53 +0700

    logo web

--- synapse-web/resources/views/lupaSandi.blade.php
+++ synapse-web/resources/views/lupaSandi.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Laravel') }} - Lupa Sandi</title>
+    <link rel="icon" type="image/png" href="{{ asset('assets/image/synapseLogo.png') }}">
     <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
--- synapse-web/resources/views/lupaSandiOTP.blade.php
+++ synapse-web/resources/views/lupaSandiOTP.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Laravel') }} - Verifikasi OTP</title>
+    <link rel="icon" type="image/png" href="{{ asset('assets/image/synapseLogo.png') }}">
     <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <style>
